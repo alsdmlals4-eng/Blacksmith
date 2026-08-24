e21ba4cf6ed9698b1541cdb680ff044c26043058
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-24 23:24:04 +0900

    feat: add task4 context api surface

--- scripts/vertical_slice/domain/vs_customer_context_packet.gd
+++ scripts/vertical_slice/domain/vs_customer_context_packet.gd
@@ -1,2 +1,31 @@
 class_name VSCustomerContextPacket
 extends RefCounted
+
+var schema_version := 1
+var customer_id := ""
+var content_id := ""
+var primary_need := ""
+var secondary_need := ""
+var known_context: Array[String] = []
+var risk := 0
+var strength := 0
+var dexterity := 0
+var constitution := 0
+var judgment := 0
+var weapon_proficiency := 0
+var related_ability := ""
+var required_function_if_explicit := ""
+var relevant_precision_axes: Array[String] = []
+var relevant_function_ids: Array[String] = []
+
+
+func to_dict() -> Dictionary:
+	return {}
+
+
+func maximum_load() -> int:
+	return 0
+
+
+func related_ability_value() -> int:
+	return 0
--- scripts/vertical_slice/resolvers/vs_customer_context_resolver.gd
+++ scripts/vertical_slice/resolvers/vs_customer_context_resolver.gd
@@ -1,2 +1,14 @@
 class_name VSCustomerContextResolver
 extends RefCounted
+
+
+func evaluate(_customer, _item, _context) -> Dictionary:
+	return {}
+
+
+func enhancement_contribution_pp(_enhancement_level: int) -> int:
+	return 0
+
+
+func proficiency_modifier_pp(_proficiency_level: int) -> int:
+	return 0
